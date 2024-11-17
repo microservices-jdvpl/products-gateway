af5777a6f797b5218b46e3b2156ab6a5eb8c57da
jdsuarez <jdsuarez@fgs.co>
2024-11-17 13:53:20 -0500

    adding microservice of orders

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,6 +6,8 @@ interface IEnvs {
   PORT: number;
   PRODUCTS_MICROSERVICES_HOST: string;
   PRODUCTS_MICROSERVICES_PORT: number;
+  ORDERS_MICROSERVICES_HOST: string;
+  ORDERS_MICROSERVICES_PORT: number;
 }
 
 const envsSchema = joi
@@ -13,6 +15,8 @@ const envsSchema = joi
     PORT: joi.number().required(),
     PRODUCTS_MICROSERVICES_HOST: joi.string().required(),
     PRODUCTS_MICROSERVICES_PORT: joi.number().required(),
+    ORDERS_MICROSERVICES_HOST: joi.string().required(),
+    ORDERS_MICROSERVICES_PORT: joi.number().required(),
   })
   .unknown(true);
 
@@ -28,4 +32,6 @@ export const envs = {
   PORT: envVars.PORT,
   PRODUCTS_MICROSERVICES_HOST: envVars.PRODUCTS_MICROSERVICES_HOST,
   PRODUCTS_MICROSERVICES_PORT: envVars.PRODUCTS_MICROSERVICES_PORT,
+  ORDERS_MICROSERVICES_HOST: envVars.ORDERS_MICROSERVICES_HOST,
+  ORDERS_MICROSERVICES_PORT: envVars.ORDERS_MICROSERVICES_PORT,
 };
